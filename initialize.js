c343722fdcf8c9c660263110c2cd05c45e12be74
Mitchel Carson <mitchel.carson@gmail.com>
2025-04-08 12:15:27 -0400

    Added initialize file that describes how to initialize a new node project.

--- initialize.js
+++ initialize.js
@@ -3,3 +3,12 @@ console.log("Initializing the application...");
 // For example, you might want to set up a database connection or load environment variables.
 
 //npm init
+
+//npm install d3 (this adds d3 to the dependecies in package.json)
+
+//npm install express (this adds express to the dependecies in package.json)
+//These packages are only stored in this directory.
+
+//You can ignore the node_modules folder. It is created when you install a package and contains all the dependencies for your project.
+
+//npm install  -- will install all the dependencies listed in package.json
